cbb44ae7b2b689540e233fdd62b4c135e67ddf9e
jaja1126-stack <jaja1126@gmail.com>
2026-09-05 14:49:11 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'fitness-log-v7';
+const CACHE_NAME = 'fitness-log-v8';
 const CORE_ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (event) => {
